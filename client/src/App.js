cf6ebfe188863c02141991ed9e8993a2ccc8e3f1
JungWho <jungwhonam@gmail.com>
2021-04-23 17:52:02 +0900

    Add get customers rest api

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,25 @@ const styles = theme => ({
   }
 });
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '나동빈',
-    'birthday': '960508',
-    'gender': '남자',
-    'job': '프로그래머'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '이순신',
-    'birthday': '961127',
-    'gender': '남자',
-    'job': '디자이너'
-  }
-]
-
 class App extends React.Component {
+
+  state = {
+    customers: ""
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const res = await fetch('/api/customers');
+    const body = await res.json();
+    return body;
+  }
+
+
   render() {
     // 위에서 적용한 styles를 사용하기 위해 classes 변수 선언
     const { classes } = this.props;
@@ -65,9 +56,10 @@ class App extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
-              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-            })}
+            {this.state.customers ? this.state.customers.map(c => {
+                return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+              }) : ""
+            }
           </TableBody>
         </Table>
       </Paper>
