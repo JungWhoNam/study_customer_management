e54dfbd94bfff7a12c9106dd9fe43e3d38cb52b6
JungWho <jungwhonam@gmail.com>
2021-04-23 16:58:12 +0900

    Integrate material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 import Customer from './components/Customer';
+import { withStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
+
+// add CSS styles
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
 
 const customers = [
   {
@@ -20,7 +39,7 @@ const customers = [
   },
   {
     'id': 3,
-    "image": "https://placeimg.com/64/64/3",
+    'image': 'https://placeimg.com/64/64/3',
     'name': '이순신',
     'birthday': '961127',
     'gender': '남자',
@@ -30,14 +49,32 @@ const customers = [
 
 class App extends React.Component {
   render() {
+    // 위에서 적용한 styles를 사용하기 위해 classes 변수 선언
+    const { classes } = this.props;
     return (
-      <div>
-        {customers.map(c => {
-          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-        })}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+//https://material-ui.com/styles/basics/
+// material ui에서 style 적용하는 세가지 방법중 higher-oder compoent api 방식 사용
+export default withStyles(styles)(App);
